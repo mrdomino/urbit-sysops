3b7d6117065994417796e65ec749ea4f7af4281a
Jōshin <git@wholezero.org>
2021-12-24 23:51:39 -0800

    install, make-urbit

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -eux
+
+install() {
+  sudo install -m 0755 -o root -g root sbin/urbit-meld /usr/local/sbin
+  sudo install -m 0755 -o root -g root sbin/make-urbit /usr/local/sbin
+  sudo install -m 0644 -o root -g root systemd/urbit@.service /etc/systemd/system
+  sudo install -m 0644 -o root -g root systemd/urbit-meld@.service /etc/systemd/system
+  sudo install -m 0644 -o root -g root systemd/urbit-meld@.timer /etc/systemd/system
+  sudo mkdir -p /etc/urbit
+}
+
+type install
+read ok?
+install
