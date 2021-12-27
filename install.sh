91fb3960ed99c895dee509a62fef9f9457e836ee
Jōshin <git@wholezero.org>
2021-12-27 11:38:38 -0800

    add urbit-exporter

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -eux
 install() {
   sudo install -m 0755 -o root -g root sbin/urbit-meld /usr/local/sbin
   sudo install -m 0755 -o root -g root sbin/make-urbit /usr/local/sbin
+  sudo install -m 0755 -o root -g root sbin/urbit-exporter /usr/local/sbin
   sudo install -m 0644 -o root -g root systemd/urbit@.service /etc/systemd/system
   sudo install -m 0644 -o root -g root systemd/urbit-meld@.service /etc/systemd/system
   sudo install -m 0644 -o root -g root systemd/urbit-meld@.timer /etc/systemd/system
@@ -11,5 +12,5 @@ install() {
 }
 
 type install
-read ok?
+read enter_to_continue
 install
