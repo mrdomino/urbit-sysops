cf0d513e7109a3dc96a1c69a6c5699942cd2287d
Jōshin <git@wholezero.org>
2021-12-27 11:45:05 -0800

    systemd script

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 set -eux
 
-install() {
+main() {
   sudo install -m 0755 -o root -g root sbin/urbit-meld /usr/local/sbin
   sudo install -m 0755 -o root -g root sbin/urbit-meld-docker /usr/local/sbin
   sudo install -m 0755 -o root -g root sbin/make-urbit /usr/local/sbin
   sudo install -m 0755 -o root -g root sbin/urbit-exporter /usr/local/sbin
   sudo install -m 0644 -o root -g root systemd/urbit@.service /etc/systemd/system
+  sudo install -m 0644 -o root -g root systemd/urbit-exporter@.service /etc/systemd/system
   sudo install -m 0644 -o root -g root systemd/urbit-meld@.service /etc/systemd/system
   sudo install -m 0644 -o root -g root systemd/urbit-meld@.timer /etc/systemd/system
   sudo mkdir -p /etc/urbit
+  sudo adduser --system --shell /usr/sbin/nologin --gecos "" \
+               --disabled-password --no-create-home urbit-exporter
 }
 
-type install
+type main
 read enter_to_continue
-install
+main
