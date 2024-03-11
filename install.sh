e721084b5e58d7f907eb591cd378bd700d5fcf38
Jōshin <git@wholezero.org>
2024-03-11 23:19:38 +0000

    Add urbit-pack target
    Word on the street is that pack is better than meld these days (I have
    heard that it's easier on SSDs, and also that it does a better job, but
    have not verified either of these.) So add a script for it, analogous to
    urbit-meld.

--- install.sh
+++ install.sh
@@ -8,10 +8,15 @@ main() {
   sudo install -m 0755 -o root -g root sbin/urbit-exporter /usr/local/sbin
   sudo install -m 0755 -o root -g root sbin/urbit-meld-docker-all /usr/local/sbin
   sudo install -m 0755 -o root -g root sbin/urbit-meld-docker /usr/local/sbin
+  sudo install -m 0755 -o root -g root sbin/urbit-pack-docker-all /usr/local/sbin
+  sudo install -m 0755 -o root -g root sbin/urbit-pack-docker /usr/local/sbin
   sudo install -m 0755 -o root -g root sbin/urbit-meld /usr/local/sbin
+  sudo install -m 0755 -o root -g root sbin/urbit-pack /usr/local/sbin
   sudo install -m 0644 -o root -g root systemd/urbit-exporter@.service /etc/systemd/system
   sudo install -m 0644 -o root -g root systemd/urbit-meld@.service /etc/systemd/system
   sudo install -m 0644 -o root -g root systemd/urbit-meld@.timer /etc/systemd/system
+  sudo install -m 0644 -o root -g root systemd/urbit-pack@.service /etc/systemd/system
+  sudo install -m 0644 -o root -g root systemd/urbit-pack@.timer /etc/systemd/system
   sudo install -m 0644 -o root -g root systemd/urbit@.service /etc/systemd/system
   sudo mkdir -p /etc/urbit
   sudo adduser --system --shell /usr/sbin/nologin --gecos "" \
