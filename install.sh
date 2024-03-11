4bc5705b98f34c45e16d23a868fe67753f5a046e
Jōshin <git@wholezero.org>
2024-03-11 23:12:01 +0000

    sort install.sh

--- install.sh
+++ install.sh
@@ -3,16 +3,16 @@ set -eu
 
 main() {
   sudo install -m 0755 -o root -g root bin/urbit-fg /usr/local/bin
-  sudo install -m 0755 -o root -g root sbin/urbit-meld /usr/local/sbin
-  sudo install -m 0755 -o root -g root sbin/urbit-meld-docker /usr/local/sbin
-  sudo install -m 0755 -o root -g root sbin/urbit-meld-docker-all /usr/local/sbin
-  sudo install -m 0755 -o root -g root sbin/make-urbit /usr/local/sbin
   sudo install -m 0755 -o root -g root sbin/make-urbit-docker /usr/local/sbin
+  sudo install -m 0755 -o root -g root sbin/make-urbit /usr/local/sbin
   sudo install -m 0755 -o root -g root sbin/urbit-exporter /usr/local/sbin
-  sudo install -m 0644 -o root -g root systemd/urbit@.service /etc/systemd/system
+  sudo install -m 0755 -o root -g root sbin/urbit-meld-docker-all /usr/local/sbin
+  sudo install -m 0755 -o root -g root sbin/urbit-meld-docker /usr/local/sbin
+  sudo install -m 0755 -o root -g root sbin/urbit-meld /usr/local/sbin
   sudo install -m 0644 -o root -g root systemd/urbit-exporter@.service /etc/systemd/system
   sudo install -m 0644 -o root -g root systemd/urbit-meld@.service /etc/systemd/system
   sudo install -m 0644 -o root -g root systemd/urbit-meld@.timer /etc/systemd/system
+  sudo install -m 0644 -o root -g root systemd/urbit@.service /etc/systemd/system
   sudo mkdir -p /etc/urbit
   sudo adduser --system --shell /usr/sbin/nologin --gecos "" \
                --disabled-password --no-create-home urbit-exporter || true
